61087f19bc9f7229ba40ebe4fa88d8e951d6c571
Miguel Angel Gonzalez Mauricio <miguelangel.gonzalezmaur@my.tccd.edu>
2023-11-09 19:44:16 -0600

    new

---
+++ Labs/Lab8/Recursion/Recursion/Recursion.cpp
@@ -0,0 +1,19 @@
+/*#include<iostream>
+using namespace std;
+void message(int);
+int main() {
+	message(5);
+	return 0;
+}
+void message(int times)
+{
+	cout << "message called with " << times << " in times.\n";
+	if (times > 0)
+	{
+		cout << "This is a recursive function.\n";
+		message(times - 1);
+	}
+	cout << "message returning with " << times;
+	cout << " in times.\n";
+ }
+ */
