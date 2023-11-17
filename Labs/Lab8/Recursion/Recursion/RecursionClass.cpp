b3f3bf6af0273405d3cf486cb79682c9bb626474
Miguel Angel Gonzalez Mauricio <miguelangel.gonzalezmaur@my.tccd.edu>
2023-11-16 19:26:43 -0600

    ok

--- Labs/Lab8/Recursion/Recursion/RecursionClass.cpp
+++ Labs/Lab8/Recursion/Recursion/RecursionClass.cpp
@@ -1,34 +1,34 @@
-/*//Calculate a factorial iteratively and recursevely
-/*#include<iostream>
-/*using namespace std;
-/*long fact(long n);
-/*long fact2(long n);
-/*int fib(int n);
-/*
-/*int main() {
-/*	int input;
-/*    cout << "Enter a number: ";
-/*    cin >> input;
-/*    cout << "The fibonacci sequence of " << input << " is " << fib(input);
-/*
-/*}
-/*
-/*int fib(int n) {
-/*	if (n <= 1)return n;
-/*	//if (n == 0)return 0;
-/*	return fib(n - 1) + fib(n - 2);
-/*}
-/*
-/*long fact2(long n) {
-/*	if (n == 1)return 1;//termination
-/*	return n * fact2(n - 1);//recursion
-/*}
-/*
-/*long fact(long n) {
-/*	long product = 1;
-/*	for (long i = n; i > 0; i--) {
-/*		product *= i;
-/*	}
-/*	return product;
-/*}
-* */
+
+//Calculate a factorial iteratively and recursevely
+#include<iostream>
+using namespace std;
+long fact(long n);
+long fact2(long n);
+int fib(int n);
+
+int main() {
+	int input;
+    cout << "Enter a number: ";
+    cin >> input;
+    cout << "The fibonacci sequence of " << input << " is " << fib(input);
+
+}
+
+int fib(int n) {
+	if (n <= 1)return n;
+	//if (n == 0)return 0;
+	return fib(n - 1) + fib(n - 2);
+}
+
+long fact2(long n) {
+	if (n == 1)return 1;//termination
+	return n * fact2(n - 1);//recursion
+}
+
+long fact(long n) {
+	long product = 1;
+	for (long i = n; i > 0; i--) {
+		product *= i;
+	}
+	return product;
+}
